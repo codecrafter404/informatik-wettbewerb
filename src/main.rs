07f2ece3a565cea26f60397fd9a1390bb95f97ec
code <code@CODE-SURFACE>
2023-12-04 11:53:41 +0100

    + c)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::io::Write;
 
+use rand::Rng;
+
 fn main() {
-    let n1 = take_input();
-    let n2 = take_input();
-    println!(
-        "{}*{} = {} | Probe: {}",
-        n1,
-        n2,
-        n1 * n2,
-        if probe(n1, n2) {
-            "successful"
-        } else {
-            "failed"
+    let mut success = 0;
+    for _ in 0..100 {
+        let n1 = rand::thread_rng().gen_range(100..1000);
+        let n2 = rand::thread_rng().gen_range(100..1000);
+        let probe = probe(n1, n2);
+        println!("{} * {} = {} | Probe: {}", n1, n2, n1 * n2, probe);
+        if probe {
+            success += 1;
         }
-    );
+    }
+    println!("----[Results]----");
+    println!("{}/100 Probes succeeded ({}%)", success, success)
+
 }
 fn probe(n1: i32, n2: i32) -> bool {
     let n1: i32 = n1
@@ -42,7 +44,7 @@ fn probe(n1: i32, n2: i32) -> bool {
 }
 fn take_input() -> i32 {
     print!("Input Number: ");
-    std::io::stdout().flush();
+    std::io::stdout().flush().unwrap();
     let mut buffer = String::new();
     std::io::stdin().read_line(&mut buffer).unwrap();
     match buffer.trim().parse::<i32>() {
